50643ba4ce7df7463766719561dffa07f23ce02d
sundaybest3 <163014658+sundaybest3@users.noreply.github.com>
2024-12-28 23:34:29 +0900

    Update NounSmart_spelling.py

--- NounSmart_spelling.py
+++ NounSmart_spelling.py
@@ -87,10 +87,10 @@ def check_plural(user_input):
 
 # Streamlit UI
 st.title("NounSmart: Spelling")
-st.markdown("There are 16 items in total. Try to get all the plural forms correct. <br> Enter your nickname to start.")
+st.markdown("There are 16 items in total. Try to get all the plural forms correct.<br>Enter your nickname to start.")
 
 # Input for nickname
-nickname = st.text_input("Enter your nickname:", key="nickname_input")
+nickname = st.text_input("Nickname:", key="nickname_input")
 
 if st.button("Start Game"):
     if nickname:
